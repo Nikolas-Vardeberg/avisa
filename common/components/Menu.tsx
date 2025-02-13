f24b3da4c6456f0a7361427688787b689e730646
niklusrook <nikolas.vardeberg1@gmail.com>
2025-02-13 12:29:57 +0100

    feat: progressbar

--- common/components/Menu.tsx
+++ common/components/Menu.tsx
@@ -2,19 +2,38 @@
 
 import { MenuIcon, Moon, Search, SearchIcon, Sun, User } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { useTheme } from "next-themes";
+import { Progress } from "./ui/progress";
 
 export default function Menu() {
-    const { setTheme, theme } = useTheme()
+    const { setTheme, theme } = useTheme();
+
+    const [scrollProgress, setScrollProgress] = useState(0);
+
+    const handleScroll = () => {
+        const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+        const progress = (window.scrollY / totalHeight) * 100;
+        setScrollProgress(progress);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     
     return(
-        <header className="bg-background border-b-2 z-10 w-full">
+        <>
+        <header className="bg-background border-b-2 z-10 w-full sticky top-0">
         <div className="py-5 px-8 max-w-[1200px] mx-auto flex items-center justify-between lg:py-7 gap-x-12">
             <Link href='/' className='flex items-center justify-center lead !text-black dark:!text-white' aria-label='Hjem'>
                 Nordvia
             </Link>
+
+           
 
             <div className="flex items-center justify-center gap-x-8">
                 <div className="w-full gap-x-2 bg-slate-200 py-3 px-4 hidden sm:flex items-center justify-center">
@@ -64,6 +83,8 @@ export default function Menu() {
 
             </div>
         </div>
+    <Progress value={scrollProgress} className="bg-yellow-100 h-1" />
     </header>
+    </>
     )
 }
